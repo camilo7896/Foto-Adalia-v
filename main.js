78d7fd0a8dc61555d52cd072ea24b584b5948bb2
camilo7896 <63164154+camilo7896@users.noreply.github.com>
2020-05-20 20:15:36 -0500

    Update main.js

--- main.js
+++ main.js
@@ -26,8 +26,8 @@ const openLightbox = (gallery, i, larges) => {
                 <img src="${larges[i]}" class="lightbox-image">
                 <figcaption>
                 <nav class="navigation">
-                <a href="#" class="lightbox-navigation prev"><i><img src="logo.png" alt="" class="logo"></i></a>
-                <a href="#" class="lightbox-navigation next"><i><img src="logo.png" alt="" class="logo"></i></a>
+                <a href="#" class="lightbox-navigation prev"><i><img src="flechas.png" alt="" class="logo"></i></a>
+                <a href="#" class="lightbox-navigation next"><i><img src="flechas.png" alt="" class="logo"></i></a>
                 </nav>
                 </figcaption>
             </figure>
