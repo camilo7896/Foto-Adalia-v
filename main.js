f41d0e154ca0ea8d901cdafb25a9823eda72fc91
camilo7896 <63164154+camilo7896@users.noreply.github.com>
2020-05-20 22:17:51 -0500

    Update main.js

--- main.js
+++ main.js
@@ -21,13 +21,13 @@ const openLightbox = (gallery, i, larges) => {
     let lightboxElement = document.createElement('div')
     lightboxElement.innerHTML = `
         <div class="lightbox-overlay">
-        <div class="close-modal"><i> <img src="exit.png" alt=""></i></div>
+        <div class="close-modal"> <img src="exit.png" alt="" class="logo"><i class="zmdi zmdi-close"></i></div>
             <figure class="lightbox-container">
                 <img src="${larges[i]}" class="lightbox-image">
                 <figcaption>
                 <nav class="navigation">
-                <a href="#" class="lightbox-navigation prev"><i> <img src="flechas.png" alt=""></i></a>
-                <a href="#" class="lightbox-navigation next"><i> <img src="flechas.png" alt=""></i></a>
+                <a href="#" class="lightbox-navigation prev"><i class="zmdi zmdi-chevron-left"></i></i></a>
+                <a href="#" class="lightbox-navigation next"><i class="zmdi zmdi-chevron-right"></i></a>
                 </nav>
                 </figcaption>
             </figure>
